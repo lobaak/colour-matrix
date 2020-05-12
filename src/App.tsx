d90f6c8013f491824d8a6d621e2d80d0d93855aa
Nathan Thorpe <nath.thorpe@gmail.com>
2020-05-12 17:32:35 +1000

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+interface RGB {
+  red: number;
+  blue: number;
+  green: number;
+}
+
+const SIZE = 8;
+
+const Color: React.FC<RGB> = ({ red, green, blue }) => {
+  const style = {
+    backgroundColor: `rgba(${red}, ${green}, ${blue} 1)`,
+    width: '1px',
+    height: '1px',
+    float: `left`,
+  } as React.CSSProperties;
+
+  return <div style={style} />;
+};
+
+const getRgbValues = () => {
+  const count = 256;
+  let colors: RGB[] = [];
+
+  for (let red = 8; red <= count; red += 8) {
+    for (let blue = 8; blue <= count; blue += 8) {
+      for (let green = 8; green <= count; green += 8) {
+        const rgb = { red, green, blue };
+        colors.push(rgb);
+      }
+    }
+  }
+
+  return colors;
+};
 
 function App() {
+  const [colors, setColors] = React.useState<RGB[]>([]);
+
+  const canvas = React.useRef<HTMLCanvasElement>(null);
+
+  React.useEffect(() => {
+    const c = getRgbValues();
+    setColors(c);
+  }, []);
+
+  React.useLayoutEffect(() => {
+    if (canvas.current) {
+      const ctx = canvas.current.getContext('2d');
+
+      if (ctx) {
+        let x = 0;
+        let y = 0;
+
+        colors.forEach(({ red, green, blue }, i) => {
+          if (x >= 1024) {
+            x = 0;
+            y += SIZE;
+          }
+
+          ctx.beginPath();
+          ctx.fillRect(x, y, SIZE, SIZE);
+          ctx.fillStyle = `rgb(${red},${green},${blue})`;
+
+          x += SIZE;
+        });
+      }
+    }
+  }, [colors]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <canvas ref={canvas} width="1024" height="1000" />
     </div>
   );
 }
